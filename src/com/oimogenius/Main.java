fd665e42ae93e108a2562d23dfdabb640887078f
oimogenius <oimo.genius@gmail.com>
2024-05-16 15:39:44 -0700

    initial commit

---
+++ src/com/oimogenius/Main.java
@@ -0,0 +1,11 @@
+package com.oimogenius;
+
+public class Main {
+    // コメント
+    // プログラムには影響しないのでメモとして使える
+//    String name = "oimo";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
